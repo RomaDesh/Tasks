80d0082003a61e5bcb3a4ceba2ac2b9e3af05584
RomaDesh <34339140+RomaDesh@users.noreply.github.com>
2017-12-07 16:03:10 +0300

    Update Source.cpp

--- T06/T06/Source.cpp
+++ T06/T06/Source.cpp
@@ -4,12 +4,12 @@ void main()
 {
 	setlocale(LC_ALL, "Ru");
 	int a[MAX][MAX], i = 0, j = 0, n;
-	cout << "Vvedite 4islo ";
+	cout << "Введите порядок матрицы ";
 	cin >> n;
 	for (i = 0; i < n; i++)
 		for (j = 0; j < n; j++)
 		{
-			cout << "Vvedite element " << i + 1 << " stroki, " << j + 1 << " stolbca ";
+			cout << "Введите элемент " << i + 1 << " строки, " << j + 1 << " столбца ";
 			cin >> a[i][j];
 		}
 	func(a, n);
